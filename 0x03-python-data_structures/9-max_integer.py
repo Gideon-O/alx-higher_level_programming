a51041a741d5dacf022514f9773255ddad8b66ad
Gideon-O <osorogideon10@gmail.com>
2022-06-07 17:56:39 +0000

    Max element in list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    n = len(my_list)
-    if n <= 0:
-        return None
-    else:
-        max_el = my_list[0]
-        for i in my_list:
-            if max_el < my_list[i]:
-                max_el = my_list[i]
-        return max_el
-
-
-if __name__ == "__main__":
-    max_integer(my_list)
+    if len(my_list) == 0:
+        return ("None")
+    x = my_list[0]
+    for i in my_list:
+        if i > x:
+            x = i
+    return (x)
