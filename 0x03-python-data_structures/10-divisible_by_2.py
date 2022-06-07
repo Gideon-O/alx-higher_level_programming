69278bd6fd97cec4177a28092f54b31668ec8b36
Gideon-O <osorogideon10@gmail.com>
2022-06-07 17:58:32 +0000

    Check for divisibility by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    check_list = []
-    for i in my_list:
+    boolist = my_list[:]
+    for count, i in enumerate(my_list):
         if i % 2 == 0:
-            check_list.append('True')
+            boolist[count] = True
         else:
-            check_list.append('False')
-    return check_list
-
-
-if __name__ == "__main__":
-    divisible_by_2(my_list)
+            boolist[count] = False
+    return(boolist)
