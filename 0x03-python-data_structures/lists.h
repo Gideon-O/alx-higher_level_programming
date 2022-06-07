d4dd5aeed75029c3b04b114ce256809a2c5995d1
Gideon Osoro <89097948+Gideon-O@users.noreply.github.com>
2022-06-07 21:07:39 +0300

    Update lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,22 +1,22 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
-  * @n: Integer
-  * @next: points to next node
-  * 
-  * Description: Singly linked node structure
-  * for project
-  */
-
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s;
-}listint_t;
+  int n;
+  struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
