161d299f301e3c79691900addff1302dc2b67af3
Gideon-O <osorogideon10@gmail.com>
2022-06-07 11:23:48 +0000

    Multi-type tuple

---
+++ 0x03-python_data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    str_len = len(sentence)
+    str_first = sentence[0]
+    data = str_len, str_first
+    return data
+
+
+if __name__ == "__main__":
+    multiple_returns(sentence)
