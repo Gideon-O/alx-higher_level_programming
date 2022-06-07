ca33f79bd89640087d58bda7111ecfe8fc1398de
Gideon-O <osorogideon10@gmail.com>
2022-06-07 17:05:57 +0000

    Update directory name

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix[x][]:
+        for y in matrix[x][y]:
+            print("{:d}".format(matrix[x][y]))
+
+
+if __name__ == "__main__":
+    print_matrix_integer(matrix=[[]])
