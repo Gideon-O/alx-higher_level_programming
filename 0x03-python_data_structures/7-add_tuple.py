72c0b16021d093cced0c80a91b4c3724f569075b
Gideon-O <osorogideon10@gmail.com>
2022-06-07 11:19:51 +0000

    Sum tuples

---
+++ 0x03-python_data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b()):
+    sum_tuple = ()
+    for i in range(0, 2):
+        sum_tuple.append(tuple_a[i] + tuple_b[i])
+
+
+if __name__ == "__name__":
+  add_tuple(tuple_a=(), tuple_b())
