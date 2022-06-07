3816d42180615fc34f5dee381058dc82bc9eedec
Gideon-O <osorogideon10@gmail.com>
2022-06-07 17:55:02 +0000

    Multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    str_len = len(sentence)
-    str_first = sentence[0]
-    data = ()
-    if str_len <= 0:
-        data = str_len, None
-    else:
-        data = str_len, str_first
-    return data
-
-
-if __name__ == "__main__":
-    multiple_returns(sentence)
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else "None"
+    tup = length, first_char
+    return(tup)
