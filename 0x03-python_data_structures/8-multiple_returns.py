31e30c6a4b45937dcdfdcf6ef15916efd61c4576
Gideon-O <osorogideon10@gmail.com>
2022-06-07 11:32:52 +0000

    String length

--- 0x03-python_data_structures/8-multiple_returns.py
+++ 0x03-python_data_structures/8-multiple_returns.py
@@ -2,7 +2,11 @@
 def multiple_returns(sentence):
     str_len = len(sentence)
     str_first = sentence[0]
-    data = str_len, str_first
+    data = ()
+    if str_len <= 0:
+        data = str_len, None
+    else:
+        data = str_len, str_first
     return data
 
 
