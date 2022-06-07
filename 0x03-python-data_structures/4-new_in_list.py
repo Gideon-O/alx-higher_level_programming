d4db5517f652ed852d271971cbdf25fd6136fc8a
Gideon-O <osorogideon10@gmail.com>
2022-06-07 17:42:21 +0000

    New element in list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    return my_list.insert(idx, element).remove(idx + 1)
-
-
-if __name__ == "__main__":
-    new_in_list(my_list, idx, element)
+    tmp_list = my_list[:]
+    if 0 <= idx < len(my_list):
+        tmp_list[idx] = element
+        return(tmp_list)
+    return(my_list)
