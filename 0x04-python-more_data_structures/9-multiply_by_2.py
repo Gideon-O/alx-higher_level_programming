a08409bb13a0e50d2ad7dfc1116598beea54d0ce
Gideon-O <osorogideon10@gmail.com>
2022-06-08 18:58:59 +0000

    Double the values

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] *= 2
+    new_dictionary = a_dictionary.copy()
+    for key in new_dictionary.keys():
+        new_dictionary[key] *= 2
     return (a_dictionary)
