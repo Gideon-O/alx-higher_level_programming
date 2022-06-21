6a874e849a119d8f8b1eb20cb45f1596c503c389
Gideon-O <osorogideon10@gmail.com>
2022-06-21 03:56:35 +0000

    Print with exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """
+    Prints the elements of a list
+    Returns the number of elements printed
+    """
     int y = 0
 
-    try:
-        for y = 0, i in my_list:
-            print("{}".format(i), end="")
-            y++;
-        return (y)
-    except:
-        print("An error occured")
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            y += 1
+        except:
+            continue
+    print()
+    return y
