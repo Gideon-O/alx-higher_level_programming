f5a890bbb14127f5ce18184b91e068203873f46e
Gideon_O <osorogideon10@gmail.com>
2022-06-21 18:01:28 +0300

    Update file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             printed += 1
-        except:
-            continue
-    print()
+    except IndexError:
+        print("Invalid Index!")
+    finally:
+        print()
     return printed
