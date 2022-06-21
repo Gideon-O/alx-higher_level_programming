88b25fb0ad7664bc5e538a004b43a8800756126f
Gideon_O <osorogideon10@gmail.com>
2022-06-21 18:38:53 +0300

    Divide to integers

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,11 +4,10 @@ def safe_print_division(a, b):
     Function to multuply two integers
     Returns the value of the division, otherwise returns None
     """
-    div_int = None
     try:
-        div_int = a / b
-        return div_int
+        div_int = a / b 
     except (ZeroDivisionError, ValueError):
-        return div_int
+        div_int = None
     finally:
         print("Inside Result: {:d}".format(div_int))
+    return div_int
