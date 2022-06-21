6d849143b6981d3f0508ffb2f20315342866b638
Gideon_O <osorogideon10@gmail.com>
2022-06-21 20:37:28 +0300

    Raise exception with message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    A function that raises a name exception
+    The message is printed as the exception message
+    """
+    raise NameError(message)
