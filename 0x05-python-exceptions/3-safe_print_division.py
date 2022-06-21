b35ef65bbc68dd2e4e5a010ea972de8745be5326
Gideon_O <osorogideon10@gmail.com>
2022-06-21 18:44:42 +0300

    Divide two integers

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -9,5 +9,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         div_int = None
     finally:
-        print("Inside Result: {:d}".format(div_int))
+        print("Inside Result: {}".format(div_int))
     return div_int
