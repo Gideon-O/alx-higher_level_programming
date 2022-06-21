aabe07e48ea0b1d2972018051e161a6f78b24a6f
Gideon_O <osorogideon10@gmail.com>
2022-06-21 17:57:44 +0300

    Update file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             printed += 1
-        except NameError:
+        except:
             continue
     print()
     return printed
