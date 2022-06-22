5c297a6dc2a463ab2f1422d873c5a936181fcfdb
Gideon Osoro <89097948+Gideon-O@users.noreply.github.com>
2022-06-22 21:36:50 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,13 +11,13 @@ class Square:
     Attributes - size
     Method - area
     """
-    def __init__(self, size=0):
-        size.__size = size
+    def __init__(self, size=0):  # init allow square class to be used
+        self.__size = size   # asign private instance attribute size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
-        ar = self.__size * self.__size
-        return ar
+        area = self.__size * self.__size
+        return area
