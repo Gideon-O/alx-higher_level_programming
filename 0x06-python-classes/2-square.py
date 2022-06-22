33e942f389e046497cca235a29031096d358a28a
Gideon_O <osorogideon10@gmail.com>
2022-06-22 21:22:08 +0300

    Mind size of square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,7 +11,8 @@ class Square:
     Attribute - size(private)
     """
     def __init__(self, size=0):
-        try:
-            size.__size = size
-        except TypeError:
-            print("size must be an integer")
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
