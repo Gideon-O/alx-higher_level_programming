a31f3f5a643d4b61ee3797eca483046564d973ba
Gideon_O <osorogideon10@gmail.com>
2022-06-22 21:02:18 +0300

    Square with value

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+@Author: Gideon Osoro
+ALX School
+"""
+
+
+class Square:
+    """
+    Square class definition
+    Attributes - size
+    """
+    def __init__(self, size):
+        self.__size = size
