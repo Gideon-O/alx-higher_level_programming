3ac20610d3838dc1ffbeffeffcab3319fff591c8
Gideon Osoro <89097948+Gideon-O@users.noreply.github.com>
2022-06-22 21:50:39 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,7 +10,7 @@ class Square:
     Square class definition
     Has the 'getter' and 'setter' concepts covered
     """
-    def __init__(self, seize=o):
+    def __init__(self, size=0):
         self.__size = size
 
     @property
@@ -19,6 +19,7 @@ class Square:
 
     @size.setter
     def size(self, value):
+        self.__size = value
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
